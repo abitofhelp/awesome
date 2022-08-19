63e333cbd37cf80dfb7060a14901464a8e21b1d7
mgingios <mike.gardner@ingios.com>
2022-08-18 21:35:11 -0700

    Working gRPC server without the grpc-gateway proxy.  It uses bsr remote code generation for its services and protobuffer definitions as a Go package.

--- go.mod
+++ go.mod
@@ -3,6 +3,22 @@ module github.com/abitofhelp/awesome
 go 1.19
 
 require (
-	go.buf.build/protocolbuffers/go/abitofhelp/abcdapis v1.3.1
-	google.golang.org/protobuf v1.28.1
+	github.com/joho/godotenv v1.4.0
+	github.com/labstack/gommon v0.3.1
+	google.golang.org/grpc v1.32.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.buf.build/grpc/go/abitofhelp/abcdapis v1.4.5 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/genproto v0.0.0-20200423170343-7949de9c1215 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
---
+++ server/runner/run.go
@@ -0,0 +1,72 @@
+package runner
+
+import (
+	"context"
+	"fmt"
+	"github.com/abitofhelp/awesome/config"
+	logger "github.com/labstack/gommon/log"
+	enumsv1 "go.buf.build/grpc/go/abitofhelp/abcdapis/enums/v1"
+	messagesv1 "go.buf.build/grpc/go/abitofhelp/abcdapis/messages/v1"
+	awesomev1 "go.buf.build/grpc/go/abitofhelp/abcdapis/services/awesome/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"net"
+	"time"
+)
+
+func Run(appcfg *config.AppConfig) error {
+
+	server := grpc.NewServer()
+
+	svc, err := NewService()
+	if err != nil {
+		panic(err)
+	}
+
+	awesomev1.RegisterAwesomeServiceServer(server, svc)
+	reflection.Register(server)
+
+	con, err := net.Listen("tcp", fmt.Sprintf("%s:%d", appcfg.Runtime.Host, appcfg.Runtime.GrpcPort))
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Printf(">>>> Starting gRPC project service on %s...\n", con.Addr().String())
+	return server.Serve(con)
+}
+
+type Server struct {
+	awesomev1.AwesomeServiceServer
+}
+
+func (s Server) FindReportByPetName(
+	ctx context.Context,
+	in *awesomev1.FindReportByPetNameRequest) (*awesomev1.FindReportByPetNameResponse, error) {
+	return s.findReportByPetName(ctx, in)
+}
+
+func (s Server) findReportByPetName(
+	ctx context.Context,
+	in *awesomev1.FindReportByPetNameRequest,
+	opts ...grpc.CallOption) (*awesomev1.FindReportByPetNameResponse, error) {
+
+	return &awesomev1.FindReportByPetNameResponse{
+		Report: &messagesv1.Report{
+			AccessTier:   enumsv1.AccessTier_ACCESS_TIER_COOL,
+			Archived:     true,
+			GeneratedUtc: timestamppb.New(time.Now().UTC()),
+			Pet: &messagesv1.Pet{
+				BirthdayUtc: timestamppb.New(time.Now().UTC()),
+				Name:        "Lassie",
+			},
+			Title: "Lassie's Report",
+			Uri:   "http://reports.com/lassie",
+		},
+		Privacy: awesomev1.Privacy_PRIVACY_NONE,
+	}, nil
+}
+
+func NewService() (*Server, error) {
+	return &Server{}, nil
+}
